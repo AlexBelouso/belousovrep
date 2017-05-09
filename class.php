fca4c819dfa4579b6e8003535be8fefc5c3c9d53
AlexBelouso <oleksiy.belousov@gmail.com>
2017-05-09 18:19:09 +0300

    Delete class.php

--- class.php
+++
@@ -1,8 +0,0 @@
-<?
-         $probel = explode(" ", $shortMe);
-        foreach ($a as &$b) {
-            if (strlen($b) > 6){ 
-            $b = substr($b,0,6)."*";
-            }
-        }
-        return(implode(" ", $probel));
